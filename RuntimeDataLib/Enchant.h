4bc7056ffb12fbdf7dd69955b8617193c97a71ee
notestsoft <notestsoft@proton.me>
2024-03-10 01:45:57 +0100

    Migrate enchant file format and add xml query precondition macro

--- RuntimeDataLib/Enchant.h
+++ RuntimeDataLib/Enchant.h
@@ -12,9 +12,10 @@ enum {
 
 #endif
 
-RUNTIME_DATA_FILE_BEGIN(cabal.enc)
+RUNTIME_DATA_FILE_BEGIN(Enchant.enc)
 
-RUNTIME_DATA_TYPE_BEGIN(UpgradeItemBasic, "cabal.enchant.item", 16)
+RUNTIME_DATA_TYPE_BEGIN(UpgradeItemBasic, "enchant.item", 16)
+	RUNTIME_DATA_PROPERTY_PRECONDITION(Int32, Extended, "extend", 0)
 	RUNTIME_DATA_PROPERTY(RTDataItemType, Type, "type")
 	RUNTIME_DATA_TYPE_BEGIN_CHILD(UpgradeItemBasicGrade, "grade", 16)
 		RUNTIME_DATA_PROPERTY(Int32, MinGrade, "min_grade")
@@ -27,16 +28,13 @@ RUNTIME_DATA_TYPE_BEGIN(UpgradeItemBasic, "cabal.enchant.item", 16)
 RUNTIME_DATA_TYPE_END(UpgradeItemBasic)
 RUNTIME_DATA_TYPE_INDEX(UpgradeItemBasic, RTDataItemType, Type)
 
-RUNTIME_DATA_FILE_END
-
-RUNTIME_DATA_FILE_BEGIN(Enchant.enc)
-
-RUNTIME_DATA_TYPE_BEGIN(UpgradeItem, "enchantextend.item", 16)
+RUNTIME_DATA_TYPE_BEGIN(UpgradeItem, "enchant.item", 16)
+	RUNTIME_DATA_PROPERTY_PRECONDITION(Int32, Extended, "extend", 1)
 	RUNTIME_DATA_PROPERTY(RTDataItemType, Type, "type")
 	RUNTIME_DATA_TYPE_BEGIN_CHILD(UpgradeItemGrade, "grade", 16)
 		RUNTIME_DATA_PROPERTY(Int32, MinGrade, "min_grade")
 		RUNTIME_DATA_PROPERTY(Int32, MaxGrade, "max_grade")
-		RUNTIME_DATA_TYPE_BEGIN_CHILD(UpgradeItemGradeValue, "enchantextend_value", 4)
+		RUNTIME_DATA_TYPE_BEGIN_CHILD(UpgradeItemGradeValue, "enchant_value", 4)
 			RUNTIME_DATA_PROPERTY(Int32, ForceID, "force_id")
 			RUNTIME_DATA_PROPERTY_ARRAY(Int32, ForceValueFormula, "force_value", 4, ',')
 			RUNTIME_DATA_PROPERTY(Int32, ForceValueType, "value_type")
@@ -46,13 +44,13 @@ RUNTIME_DATA_TYPE_BEGIN(UpgradeItem, "enchantextend.item", 16)
 RUNTIME_DATA_TYPE_END(UpgradeItem)
 RUNTIME_DATA_TYPE_INDEX(UpgradeItem, RTDataItemType, Type)
 
-RUNTIME_DATA_TYPE_BEGIN(UpgradeLimit, "enchantextend.enchant_limit.limit_value", 16)
+RUNTIME_DATA_TYPE_BEGIN(UpgradeLimit, "enchant.enchant_limit.limit_value", 16)
 	RUNTIME_DATA_PROPERTY(Int32, ItemGrade, "item_grade")
 	RUNTIME_DATA_PROPERTY(Int32, MaxItemLevel, "limit_lv")
 RUNTIME_DATA_TYPE_END(UpgradeLimit)
 RUNTIME_DATA_TYPE_INDEX(UpgradeLimit, Int32, ItemGrade)
 
-RUNTIME_DATA_TYPE_BEGIN(UpgradeCostCore, "enchantextend.enchant_coreneeds.coreneeds", 32)
+RUNTIME_DATA_TYPE_BEGIN(UpgradeCostCore, "enchant.enchant_coreneeds.coreneeds", 32)
 	RUNTIME_DATA_PROPERTY(Int32, ItemLevel, "lv")
 	RUNTIME_DATA_PROPERTY(Int32, NormalCount, "upgrade")
 	RUNTIME_DATA_PROPERTY(Int32, PerfectCount, "perfect")
@@ -61,7 +59,7 @@ RUNTIME_DATA_TYPE_BEGIN(UpgradeCostCore, "enchantextend.enchant_coreneeds.corene
 RUNTIME_DATA_TYPE_END(UpgradeCostCore)
 RUNTIME_DATA_TYPE_INDEX(UpgradeCostCore, Int32, ItemLevel)
 
-RUNTIME_DATA_TYPE_BEGIN(UpgradeResult, "enchantextend.enchant_result.result", 32)
+RUNTIME_DATA_TYPE_BEGIN(UpgradeResult, "enchant.enchant_result.result", 32)
 	RUNTIME_DATA_PROPERTY(Int32, ItemLevel, "lv")
 	RUNTIME_DATA_PROPERTY_ARRAY(Int32, Normal, "upgrade", 8, ',')
 	RUNTIME_DATA_PROPERTY_ARRAY(Int32, Enhanced, "enhanced", 8, ',')
@@ -69,7 +67,7 @@ RUNTIME_DATA_TYPE_BEGIN(UpgradeResult, "enchantextend.enchant_result.result", 32
 RUNTIME_DATA_TYPE_END(UpgradeResult)
 RUNTIME_DATA_TYPE_INDEX(UpgradeResult, Int32, ItemLevel)
 
-RUNTIME_DATA_TYPE_BEGIN(UpgradeCostSafeguard, "enchantextend.enchant_buffer.result", 32)
+RUNTIME_DATA_TYPE_BEGIN(UpgradeCostSafeguard, "enchant.enchant_buffer.result", 32)
 	RUNTIME_DATA_PROPERTY(Int32, ItemLevel, "lv")
 	RUNTIME_DATA_PROPERTY(Int32, NormalCount, "upgrade")
 	RUNTIME_DATA_PROPERTY(Int32, EnhancedCount, "enhanced")
@@ -78,7 +76,7 @@ RUNTIME_DATA_TYPE_END(UpgradeCostSafeguard)
 RUNTIME_DATA_TYPE_INDEX(UpgradeCostSafeguard, Int32, ItemLevel)
 
 /* NOT IN USE
-RUNTIME_DATA_TYPE_BEGIN(UpgradeRate, "enchantextend.enchant_rate.result", 32)
+RUNTIME_DATA_TYPE_BEGIN(UpgradeRate, "enchant.enchant_rate.result", 32)
 	RUNTIME_DATA_PROPERTY(Int32, ItemLevel, "lv")
 	RUNTIME_DATA_PROPERTY(Int32, NormalRate1, "level1uprate")
 	RUNTIME_DATA_PROPERTY(Int32, NormalRate2, "level2uprate")
