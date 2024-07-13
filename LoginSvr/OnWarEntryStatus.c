64dc2ab4fe02572ae54e350d3fbf9715798187f4
Goldenheaven <6668776+Goldenheaven@users.noreply.github.com>
2024-07-13 21:44:19 +0100

    Fix IWarEntryStatus name

---
+++ LoginSvr/OnWarEntryStatus.c
@@ -0,0 +1,13 @@
+#include "ClientProtocol.h"
+#include "ClientProcedures.h"
+#include "ClientSocket.h"
+
+CLIENT_PROCEDURE_BINDING(WAR_ENTRY_STATUS) {
+	S2C_DATA_WAR_ENTRY_STATUS* Response = PacketBufferInit(Connection->PacketBuffer, S2C, WAR_ENTRY_STATUS);
+	Response->Unknown1[0] = 0x00;
+	Response->Unknown1[1] = 0x00;
+	Response->Unknown1[2] = 0x00;
+	Response->Unknown1[3] = 0x00;
+	Response->RecievedWarReward = 0x00;
+	SocketSend(Socket, Connection, Response);
+}
