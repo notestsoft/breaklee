c7095b980fcffdff7008c54975be4e875d6c425f
MrHick <jdborden.23@gmail.com>
2025-02-11 13:24:34 -0600

    Added logic to work for all basic chat types except types with mixed map and item links.

--- WorldSvr/OnChat.c
+++ WorldSvr/OnChat.c
@@ -54,6 +54,7 @@ CLIENT_PROCEDURE_BINDING(MESSAGE_SHOUT) {
 	// boolean to determine if chat has an item linked
 	bool hasLink = false;
 	bool has2Links = false;
+	bool has3Links = false;
 	int lastIndex = -1;
 	bool hasMapLink = false;
 
@@ -69,46 +70,65 @@ CLIENT_PROCEDURE_BINDING(MESSAGE_SHOUT) {
 			hasLink = true;
 
 		}
-	}
-	// Look for "$130#[" pattern in the payload
-	for (int i = 0; i < MessageLength - 5; i++) {
 		if (Packet->Payload[i] == 0x24 && Packet->Payload[i + 1] == 0x31 && Packet->Payload[i + 2] == 0x33 &&
 			Packet->Payload[i + 3] == 0x30 && Packet->Payload[i + 4] == 0x23 && Packet->Payload[i + 5] == 0x5B) {
 			has2Links = true;
 
 		}
+		if (Packet->Payload[i] == 0x24 && Packet->Payload[i + 1] == 0x31 && Packet->Payload[i + 2] == 0x33 &&
+			Packet->Payload[i + 3] == 0x31 && Packet->Payload[i + 4] == 0x23 && Packet->Payload[i + 5] == 0x5B) {
+			has3Links = true;
+
+		}
 	}
-	// look for map code byte in the payload. 6 bytes after ]$
-	for (int i = 0; i < MessageLength - 1; i++) {
-		if (Packet->Payload[i] == 0x5D && Packet->Payload[i + 1] == 0x24) {
-			lastIndex = i + 1;
+	if (hasLink) {
+		// look for final link in the payload and go until there is no ASCII text.
+		for (int i = 0; i < MessageLength - 1; i++) {
+			if (Packet->Payload[i] == 0x5D && Packet->Payload[i + 1] == 0x24 && (MessageLength > i + 7 && Packet->Payload[i + 7] != 0x5B)) {
+				while (Packet->Payload[i] >= 0x20 && Packet->Payload[i] <= 0x7E) {
+					i++;
+				}
+				lastIndex = i;
+				break;
+			}
 		}
 	}
 
-	// check for a possible map link = 6 byte offset after ]$
-	if (lastIndex != -1 && MessageLength > lastIndex + 6 && Packet->Payload[lastIndex + 6] != 0x30) {
+	// check for a possible map link = look at 5 byte offset after ]$
+	if (lastIndex != -1 && MessageLength > lastIndex + 5 && Packet->Payload[lastIndex + 5] != 0x00) {
 		hasMapLink = true;
 	}
 
+	// Unknown1 value must be equal to the first 2 bytes of the payload + a number offset which appears to be random per case.
 	// 1 item link = + 45
 	// 1 map link = + 33
 	// basic text = + 5
 	// two map links = + 61
 	// three map links = 89
 	// two item links = 85
+	// three item links = 125
 	// map link + item link = 94
 	// map link + item link + item link = 113
 	// item link + map link + map link = 
-	// TODO: Implement multiple links of different types like 2 items + 1 map link
+	// TODO: Implement messages with map and item links mixed
 	if (hasLink) {
-		if (!has2Links && hasMapLink) {
+		if ((!has3Links && !has2Links) && hasMapLink) {
 			Notification->Unknown1 = firstTwoBytesAsInt + 33;
 		}
-		if (!has2Links && !hasMapLink) {
+		if ((!has3Links && !has2Links) && !hasMapLink) {
 			Notification->Unknown1 = firstTwoBytesAsInt + 45;
 		}
-		if (has2Links && !hasMapLink) {
+		if ((!has3Links && has2Links) && !hasMapLink) {
 			Notification->Unknown1 = firstTwoBytesAsInt + 85;
+		}
+		if (has3Links && !hasMapLink) {
+			Notification->Unknown1 = firstTwoBytesAsInt + 125;
+		}
+		if ((!has3Links && has2Links) && hasMapLink) {
+			Notification->Unknown1 = firstTwoBytesAsInt + 61;
+		}
+		if (has3Links && hasMapLink) {
+			Notification->Unknown1 = firstTwoBytesAsInt + 89;
 		}
 	}
 	else {
